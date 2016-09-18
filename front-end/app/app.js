016c9678d0c2fb6295ca2a3407175d986cfeb483
mdang8 <mdang808@gmail.com>
2016-09-18 06:05:35 -0400

    updates

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,3 +1,3 @@
 var application = require("application");
 //application.start({ moduleName: "views/login/login" });
-application.start({ moduleName: "views/login/login" });
+application.start({ moduleName: "views/browse-activities/browse-activities" });
