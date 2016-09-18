a71845850221a92c3fbbf04d5cb4a2a90199e1eb
mdang8 <mdang808@gmail.com>
2016-09-18 03:56:43 -0400

    Added front-end stuff

---
+++ front-end/app/shared/view-models/user-view-model.js
@@ -0,0 +1,60 @@
+var config = require("../../shared/config.js");
+var fetchModule = require("fetch");
+var Observable = require("data/observable").Observable;
+
+function User(info) {
+    info = info || {};
+
+    var viewModel = new Observable({
+        username: info.username || "",
+        password: info.password || ""
+    });
+
+    viewModel.login = function() {
+        return fetchModule.fetch(config.signinUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            payload: {
+                user: {
+                    username: viewModel.get("username"),
+                    password: viewModel.get("password")
+                }
+            },
+        })
+        .then(handleErrors)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+    };
+
+    viewModel.register = function() {
+        return fetchModule.fetch(config.registerUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            payload: {
+                Email: viewModel.get("email"),
+                Username: viewModel.get("username"),
+                Password: viewModel.get("password")
+            },
+        }).then(handleErrors);
+    };
+
+    return viewModel;
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        console.log(JSON.stringify(response));
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+module.exports = User;
