d095167a3ec430930fd748d443e5fa8bb623ff0d
mdang8 <mdang808@gmail.com>
2016-09-18 06:20:25 -0400

    front end bug

--- front-end/platforms/android/src/main/assets/app/views/browse-activities/browse-activities.js
+++ front-end/platforms/android/src/main/assets/app/views/browse-activities/browse-activities.js
@@ -3,13 +3,13 @@ var Observable = require("data/observable").Observable;
 var ObservableArray = require("data/observable-array").ObservableArray;
 var page;
 
-var pageData = new Observable({
-    activityList: new ObservableArray([
-        { activity: "Groceries" },
-        { activity: "Shopping" },
-        { activity: "Exercise" }
-    ])
-});
+// var pageData = new Observable({
+//     activityList: new ObservableArray([
+//         { activity: "Groceries" },
+//         { activity: "Shopping" },
+//         { activity: "Exercise" }
+//     ])
+// });
 
 exports.selectActivity = function() {
     console.log("Activity pressed");
